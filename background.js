5dab50980fc1b8bd0ce7c9b4b5006afaaa2dc71d
Stanley Wang <stanleynwang@gmail.com>
2012-12-24 01:55:43 -0800

    Added search box that jumps to a class

--- background.js
+++ background.js
@@ -5,4 +5,24 @@ $(function () {
       $(this).addClass('title');
     }
   });
+
+  $('body').prepend(
+    ['<form ',
+     'name="search" method="post" onsubmit="return validateForm()">',
+     '<input type="text" size="5" name="department">',
+     '<input type="text" size="3" maxlength="3" name="number">',
+     '<input type="submit" value="Search">',
+     '</form>'
+    ].join('\n')
+  );
+
+  var script = document.createElement('script');
+  script.text = ['function validateForm() {',
+    'var form = document.search;',
+    'console.log(form.department.value);',
+    'form.action = "#" + form.department.value + form.number.value;',
+    'return true;',
+    '}'
+  ].join('\n');
+  document.body.appendChild(script);
 });
